d36d5d6cb4bfbbcd0f91b9f7671dcdcb87224cfc
Dolmayan <alejandrodolmayan@gmail.com>
2023-05-30 13:59:35 -0600

    Queue updated

--- src/main/java/com/java/dolmayan/dsa/excercises/Queue/Queue.java
+++ src/main/java/com/java/dolmayan/dsa/excercises/Queue/Queue.java
@@ -1,4 +1,24 @@
 package com.java.dolmayan.dsa.excercises.Queue;
 
 public class Queue {
+
+    private Node first;
+    private Node last;
+    private int lenght;
+
+    public Queue(int value){
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        lenght = 1;
+    }
+
+
+    class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
 }

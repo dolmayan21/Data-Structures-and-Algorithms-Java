a09ba353a906b519ddc696acb2a78cc143535b0c
Alex L <alejandrodolmayan@gmail.com>
2023-06-05 19:09:33 -0600

    LeetCode

--- src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/longestCommonPrefix14.java
+++ src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/longestCommonPrefix14.java
@@ -1,5 +1,7 @@
 package com.java.dolmayan.dsa.excercises.LeetCode;
 
+import java.util.ArrayList;
+
 public class longestCommonPrefix14 {
 
     public static void main(String [] args){
@@ -11,30 +13,34 @@ public class longestCommonPrefix14 {
     }
     public static String longestCommonPrefix(String[] strs) {
         String concat = "";
+        char[] current = null;
+        char[] tempArr = null;
 
         char[] temp = {};
         for(String str : strs) {
-            for (char s : str.toCharArray()) {
-
-                if (!concat.equals("")) {
-                    temp = concat.toCharArray();
-                    concat = "";
-                        for( char conc : temp){
-
-                            if(s == conc){
-                                concat += conc;
-                            }else{
-                                break;
-                            }
-
-                        }
-
-
-
-                }else{
-                    concat = str;
+            for (int i=0; i< str.length(); i++) {
+                if(tempArr == null){
+                    System.out.println("curren null");
+                    System.out.println();
+                    tempArr = str.toCharArray();
                     break;
+                }else{
+                    if(tempArr[i] == str.toCharArray()[i]){
+                        concat += concat + tempArr[i];
+                    }else if(concat.length() == 0) {
+                        return "FAILS";
+                    } else if (concat.length()>=1) {
+                        tempArr = concat.toCharArray();
+                    }
+                    for(char c: tempArr){
+                        concat += c;
+                    }
+
+                    return concat;
                 }
+
+
+
 
             }
 

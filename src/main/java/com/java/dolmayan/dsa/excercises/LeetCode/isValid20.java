3cef99c3fb976a2ae08ce6ba172f007bd01aa2ba
Dolmayan <alejandrodolmayan@gmail.com>
2023-06-08 17:58:47 -0600

    isValid LeetCode method

---
+++ src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/isValid20.java
@@ -0,0 +1,24 @@
+package com.java.dolmayan.dsa.excercises.LeetCode;
+
+import java.util.Stack;
+
+public class isValid20 {
+
+
+    public static void main(String [] args){
+        String s = "()()()";
+
+        System.out.println("That is what returns ---------->" +isValid(s));
+
+
+    }
+
+
+    public static boolean isValid(String s) {
+
+
+        return true;
+    }
+
+
+}

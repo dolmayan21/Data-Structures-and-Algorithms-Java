397352fa0bfd83d00e57c68fa55f78acf29bbc6e
Dolmayan <alejandrodolmayan@gmail.com>
2023-06-10 09:59:31 -0600

    mergeTwoLists LeetCode

---
+++ src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/mergeTwoLists21.java
@@ -0,0 +1,76 @@
+package com.java.dolmayan.dsa.excercises.LeetCode;
+
+public class mergeTwoLists21 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        /*
+        * We're assuming data
+        *
+        *  list1 [1,2,4,7,8]
+        *  list2 [1,3,4,5,5]
+        *
+        * */
+
+        if(list1 == null){ return list2;}
+        if(list2 == null){ return list1;}
+
+        ListNode head;
+        ListNode temp;
+
+        if(list1.val > list2.val){
+            temp = head = new ListNode(list2.val);
+            list2 = list2.next;
+        } else{
+            temp = head = new ListNode(list1.val);
+            list1 = list1.next;
+
+        }
+            while(list1 != null || list2 != null ){
+
+
+                if(list1 ==null ){
+                    temp.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }else if(list2 ==null ){
+                    temp.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                } else {
+
+                    if (list1.val > list2.val) {
+                        temp.next = new ListNode(list2.val);
+                        list2 = list2.next;
+                    } else {
+                        temp.next = new ListNode(list1.val);
+                        list1 = list1.next;
+
+                    }
+                }
+
+                temp = temp.next;
+            }
+
+
+        return head;
+    }
+
+      static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+          ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+
+
+}

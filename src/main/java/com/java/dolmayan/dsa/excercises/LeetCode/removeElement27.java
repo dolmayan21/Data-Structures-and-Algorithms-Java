35361ce3773925984a8d83585b54c50261680b56
Alex L <alejandrodolmayan@gmail.com>
2023-06-12 15:03:41 -0600

    Set up

---
+++ src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/removeElement27.java
@@ -0,0 +1,24 @@
+package com.java.dolmayan.dsa.excercises.LeetCode;
+
+public class removeElement27 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int removeElement(int[] nums, int val) {
+
+        int j = 0;
+
+        for(int i =0; i<nums.length; i++){
+
+        }
+
+
+
+
+        return 1;
+    }
+
+}

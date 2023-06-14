5bcd0ad1a7dfb0c8e0c81b08cc3471bcbade7e6a
Dolmayan <alejandrodolmayan@gmail.com>
2023-06-13 21:45:43 -0600

    Remove Element Updated

--- src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/removeElement27.java
+++ src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/removeElement27.java
@@ -1,24 +1,57 @@
 package com.java.dolmayan.dsa.excercises.LeetCode;
 
-public class removeElement27 {
+import java.sql.SQLOutput;
 
+public class removeElement27 {
 
     public static void main(String[] args) {
 
+      //  int[] nums = {0,1,2,2,3,0,4,2};
+
+      int[] nums = {3,2,2,3};
+
+        int val = 3;
+
+        removeElement(nums, val);
     }
-
     public static int removeElement(int[] nums, int val) {
-
         int j = 0;
 
-        for(int i =0; i<nums.length; i++){
+
+        for(int i=0; i< nums.length; i++){
+
+            System.out.println("entra a for");
+           while(nums[i] != val){
+
+
+
+               nums[j] =nums[i];
+               System.out.println(nums[j]);
+               j++;
+               i++;
+
+               System.out.println("*************   "+ i);
+               if(i == (nums.length-1)){
+                   break;
+               }
+
+
+           }
 
         }
+
+
+        for(int k : nums){
+            System.out.println(k);
+        }
+        System.out.println("**************         " + j);
 
 
 
 
         return 1;
     }
+
+
 
 }

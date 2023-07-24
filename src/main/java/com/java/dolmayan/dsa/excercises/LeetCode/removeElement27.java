4f0ae7bc75cbc62be42dce0f1b14e2ea48ef5192
Alex L <alejandrodolmayan@gmail.com>
2023-07-24 15:10:39 -0600

    removeElement27 added

--- src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/removeElement27.java
+++ src/main/java/com/java/dolmayan/dsa/excercises/LeetCode/removeElement27.java
@@ -1,24 +1,31 @@
 package com.java.dolmayan.dsa.excercises.LeetCode;
 
+import java.util.Arrays;
+
 public class removeElement27 {
 
 
     public static void main(String[] args) {
+
+       // int nums[]= {3,2,2,3};
+        int nums[]= {0,1,2,2,3,0,4,2};
+
+        int val = 2;
+        System.out.println(removeElement(nums,val));
+
 
     }
 
     public static int removeElement(int[] nums, int val) {
 
         int j = 0;
+        int i =0;
 
-        for(int i =0; i<nums.length; i++){
-
-        }
-
-
-
-
-        return 1;
+            while (i<nums.length) {
+                if(nums[i++] != val){
+                }
+            }
+            return j;
     }
 
 }

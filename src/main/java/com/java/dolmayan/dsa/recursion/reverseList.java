acd9d2253fc36c6e74a4785a540bcabf50605ed8
Alex L <alejandrodolmayan@gmail.com>
2023-07-31 09:34:02 -0600

    reverseList added

---
+++ src/main/java/com/java/dolmayan/dsa/recursion/reverseList.java
@@ -0,0 +1,38 @@
+package com.java.dolmayan.dsa.recursion;
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class reverseList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+
+}

11ffe34e53f5304e8b7da89260c6aee8b65a6623
Alex L <alejandrodolmayan@gmail.com>
2023-08-04 15:27:20 -0600

    SearchInABinarySearchTree.java added

---
+++ src/main/java/com/java/dolmayan/dsa/recursion/SearchInABinarySearchTree.java
@@ -0,0 +1,32 @@
+package com.java.dolmayan.dsa.recursion;
+
+public class SearchInABinarySearchTree {
+
+/**
+ *Definition for a binary tree node.
+ **/
+ public class TreeNode {
+
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if(root == null || root.val == val){
+            return root;
+        }
+        if(root.val > val){
+            return searchBST(root.left, val);
+        }else{
+            return searchBST(root.right, val);
+        }
+    }
+}

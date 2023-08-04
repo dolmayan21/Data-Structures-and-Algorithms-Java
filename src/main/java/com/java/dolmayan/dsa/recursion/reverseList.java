e988b1ce696fe1476fdcdf25784529a985d17da6
Dolmayan <alejandrodolmayan@gmail.com>
2023-08-04 10:29:12 -0600

    reverseList updated and explained (Recursion)

--- src/main/java/com/java/dolmayan/dsa/recursion/reverseList.java
+++ src/main/java/com/java/dolmayan/dsa/recursion/reverseList.java
@@ -14,7 +14,27 @@ package com.java.dolmayan.dsa.recursion;
 
 public class reverseList {
 
-    public class ListNode {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = null;
+
+        ListNode head = reverseList(node1);
+        printList(head);
+
+
+    }
+
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
@@ -24,7 +44,7 @@ public class reverseList {
 
 
 
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
@@ -34,5 +54,14 @@ public class reverseList {
         return p;
     }
 
+    public static void printList(ListNode node){
+
+        while(node != null){
+            System.out.println(node.val);
+            node = node.next;
+        }
+
+
+    }
 
 }
